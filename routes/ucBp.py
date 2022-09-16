cbbccce287d723cee7f8665c0700139c10bb3d0c
Vinicius Saviello <viniciussaviello@hotmail.com>
2022-09-16 02:24:45 -0300

    Aula dia 14/09/22

---
+++ routes/ucBp.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template
+from ..extensions import db
+from ..models.uc import Uc
+
+
+ucBp = Blueprint('ucBp', __name__)
+
+@ucBp.route('/uc')
+def uc_list():
+    #db.create_all()
+    ucs_query = Uc.query.all()
+    return render_template("uc_list.html",ucs=ucs_query)
+    
